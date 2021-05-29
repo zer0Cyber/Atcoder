a17fa7ff937973d65e6a6328f1b399c0ecfc190f
chiaki ogawa <chiaki.takuya486951@gmail.com>
2021-05-29 21:23:50 +0900

    四則演算の基礎

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 // メイン関数
 int main() {
-    cout << "いつも" << 2525 << endl;
-    cout << "AtCoderくん"<< endl;
+    cout << 1 + 1 << endl;
+    cout << 3 - 4 << endl;
+    cout << 2 * 3 << endl;
+    cout << 7 / 3 << endl;
 }
