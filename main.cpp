940c0125687468c29ea076706224ec8f3e117c11
chiaki ogawa <chiaki.takuya486951@gmail.com>
2021-05-29 21:26:25 +0900

    余りの計算

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 // メイン関数
 int main() {
-    cout << 1 + 1 << endl;
-    cout << 3 - 4 << endl;
-    cout << 2 * 3 << endl;
-    cout << 7 / 3 << endl;
+    cout << 7 % 3 << endl;
+    cout << 4 % 5 << endl;
 }
