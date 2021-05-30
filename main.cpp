b344739d88d1d0310dc2c71c0a904666ef9ef97d
chiaki ogawa <chiaki.takuya486951@gmail.com>
2021-05-30 18:52:51 +0900

    1.04練習問題

--- main.cpp
+++ main.cpp
@@ -6,12 +6,11 @@ using namespace std;
 // メイン関数
 int main() {
 
-    int i = 30;
-    double d = 1.5;
-    string s = "Hello";
+	  // 一年の秒数
+  int seconds = 365 * 24 * 60 * 60;
 
-  cout << i + d << endl; // 31.5
-  cout << i * d << endl; // 45
-  cout << 45 / 2 << endl; // 22 小数点以下切り捨て
-  cout << i * d / 2 << endl; // 22.5 小数点以下も残る
+  cout << seconds << endl;
+  cout << 2 * seconds << endl;
+  cout << 5 * seconds << endl;
+  cout << 10 * seconds << endl;
 }
