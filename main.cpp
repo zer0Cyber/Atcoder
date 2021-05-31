33a6fccb71380987de0648e00c7d1f40cc3b9494
chiaki ogawa <chiaki.takuya486951@gmail.com>
2021-05-31 23:41:56 +0900

    入力の基礎

--- main.cpp
+++ main.cpp
@@ -1,17 +1,12 @@
-// おまじない
 #include <bits/stdc++.h>
 using namespace std;
-// おまじない終わり
 
-// メイン関数
 int main() {
 
-    int i = 30;
-    double d = 1.5;
-    string s = "Hello";
+  int a;
 
-  cout << i + d << endl; // 31.5
-  cout << i * d << endl; // 45
-  cout << 45 / 2 << endl; // 22 小数点以下切り捨て
-  cout << i * d / 2 << endl; // 22.5 小数点以下も残る
+  // 入力を受け取る
+  cin >> a;
+
+  cout << a * 10 << endl;
 }
