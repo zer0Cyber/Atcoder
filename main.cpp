c8ab9872d8eb482bdc5e3a76cc78e554b5bf310a
chiaki ogawa <chiaki.takuya486951@gmail.com>
2021-06-01 00:07:42 +0900

    A - New Year 回答

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
+// 問題文
+// 12月30日のM時から次の年になるまでは何時間か、求めてください。
+
+// 制約
+// 1 ≦ M ≦ 23  入力は全て整数
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-  // ここに追記
-  int a, b, total;
+  int time;
 
-  cin >> a >> b;
-  total = a + b;
-  cout << total << endl;
+  cin >> time;
+
+  cout << (24 - time) + 24 << endl;
 }
