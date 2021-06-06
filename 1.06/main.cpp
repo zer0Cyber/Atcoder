7c9aa5f299d5c87ab7fba6d83779d6eea51e8f8d
chiaki ogawa <chiaki.takuya486951@gmail.com>
2021-06-06 23:26:10 +0900

    比較演算子の基礎`

--- 1.06/main.cpp
+++ 1.06/main.cpp
@@ -9,5 +9,17 @@ int main() {
     cout << "xは10より小さい" << endl;
   }
 
+  if (x >= 20) {
+    cout << "xは20以上" << endl;
+  }
+
+  if (x == 5) {
+    cout << "xは5" << endl;
+  }
+
+  if (x != 100) {
+    cout << "xは100ではない" << endl;
+  }
+
   cout << "終了" << endl;
 }
