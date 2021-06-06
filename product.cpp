4ae1dbd53bfa03e66448baa47cc4975eeea17281
chiaki ogawa <chiaki.takuya486951@gmail.com>
2021-06-07 04:23:04 +0900

    productを追加

---
+++ product.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  if (a % 2 != 0 && b % 2 != 0) {
+    cout << "Odd" << endl;
+  } else {
+    cout << "Even" << endl;
+  }
+
+}
