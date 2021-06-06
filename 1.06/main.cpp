d21299f5319eb9c101b9c502a8154fc18894fd20
chiaki ogawa <chiaki.takuya486951@gmail.com>
2021-06-06 23:33:38 +0900

    if文のネスト

--- 1.06/main.cpp
+++ 1.06/main.cpp
@@ -5,16 +5,11 @@ int main() {
   int x, y;
   cin >> x >> y;
 
-  if (!(x == y)) {
-    cout << "xとyは等しくない" << endl;
-  }
-
-  if (x == 10 && y == 10) {
-    cout << "xとyは10" << endl;
-  }
-
-  if (x == 0 || y == 0) {
-    cout << "xかyは0" << endl;
+  if (x == 10) {
+    cout << "xは10" << endl;
+    if (y == 10) {
+      cout << "yも10" << endl;
+    }
   }
 
   cout << "終了" << endl;
