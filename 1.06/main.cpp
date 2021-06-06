9199da07b3fae92b32f158997eb09f8e8a0ca72b
chiaki ogawa <chiaki.takuya486951@gmail.com>
2021-06-06 23:27:13 +0900

    論理演算子の基礎

--- 1.06/main.cpp
+++ 1.06/main.cpp
@@ -2,23 +2,19 @@
 using namespace std;
 
 int main() {
-  int x;
-  cin >> x;
+  int x, y;
+  cin >> x >> y;
 
-  if (x < 10) {
-    cout << "xは10より小さい" << endl;
+  if (!(x == y)) {
+    cout << "xとyは等しくない" << endl;
   }
 
-  if (x >= 20) {
-    cout << "xは20以上" << endl;
+  if (x == 10 && y == 10) {
+    cout << "xとyは10" << endl;
   }
 
-  if (x == 5) {
-    cout << "xは5" << endl;
-  }
-
-  if (x != 100) {
-    cout << "xは100ではない" << endl;
+  if (x == 0 || y == 0) {
+    cout << "xかyは0" << endl;
   }
 
   cout << "終了" << endl;
