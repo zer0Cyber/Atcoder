838e8f0f388c073ae5f3ed3b4c3447d73866f06a
chiaki ogawa <chiaki.takuya486951@gmail.com>
2021-06-07 04:24:39 +0900

    productを修正

--- product.cpp
+++ product.cpp
@@ -5,10 +5,10 @@ int main() {
   int a, b;
   cin >> a >> b;
 
-  if (a % 2 != 0 && b % 2 != 0) {
-    cout << "Odd" << endl;
-  } else {
+  if (a * b % 2 == 0) {
     cout << "Even" << endl;
+  } else {
+    cout << "Odd" << endl;
   }
 
 }
